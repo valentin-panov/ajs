ae95e9fc6b518e256f0153973b68f157bd475294
Valentin Panov <valentin.panov@me.com>
2021-02-07 23:27:08 +0300

    step 6

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"), // каталог для результатов сборки
+    filename: "index.js", // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
+    library: "ajs", // название нашей библиотеки
+    libraryTarget: "umd", // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
+    libraryExport: "default", // экспортируется имя default
+    globalObject: "this", // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
+    // ... next 2 lines we r fighting with Error: Universal Chunk Loading is not implemented yet at EnableChunkLoadingPlugin.apply
+    chunkLoading: false,
+    wasmLoading: false,
+    // ...
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
